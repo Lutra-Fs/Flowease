d67335b641548af9be5257ce75c2af170a296549
ZHANG Bo <zhangbo163126@outlook.com>
2021-11-21 01:36:30 +1100

    Format code, remove server commands

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const {Player} = require('discord-player');
 const client = new Client();
 client.commands = new Discord.Collection();
 
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commandFiles = fs.readdirSync('./commands').
+    filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
   const command = require(`./commands/${file}`);
@@ -19,15 +20,18 @@ console.log(client.commands);
 const player = new Player(client);
 
 player.on('error', (queue, error) => {
-  console.log(`[${queue.guild.name}] Error emitted from the queue: ${error.message}`);
+  console.log(
+      `[${queue.guild.name}] Error emitted from the queue: ${error.message}`);
 });
 
 player.on('connectionError', (queue, error) => {
-  console.log(`[${queue.guild.name}] Error emitted from the connection: ${error.message}`);
+  console.log(
+      `[${queue.guild.name}] Error emitted from the connection: ${error.message}`);
 });
 
 player.on('trackStart', (queue, track) => {
-  queue.metadata.send(`▶ | Started playing: **${track.title}** in **${queue.connection.channel.name}**!`);
+  queue.metadata.send(
+      `▶ | Started playing: **${track.title}** in **${queue.connection.channel.name}**!`);
 });
 
 player.on('trackAdd', (queue, track) => {
@@ -35,7 +39,8 @@ player.on('trackAdd', (queue, track) => {
 });
 
 player.on('botDisconnect', queue => {
-  queue.metadata.send('❌ | I was manually disconnected from the voice channel, clearing queue!');
+  queue.metadata.send(
+      '❌ | I was manually disconnected from the voice channel, clearing queue!');
 });
 
 player.on('channelEmpty', queue => {
@@ -51,7 +56,7 @@ client.once('ready', async () => {
 });
 
 client.on('ready', function() {
-  client.user.setActivity(config.activity, { type: config.activityType });
+  client.user.setActivity(config.activity, {type: config.activityType});
 });
 
 client.once('reconnecting', () => {
@@ -66,16 +71,15 @@ client.on('messageCreate', async message => {
   if (message.author.bot || !message.guild) return;
   if (!client.application?.owner) await client.application?.fetch();
 
-  if (message.content === '!deploy' && message.author.id === client.application?.owner?.id) {
-    await message.guild.commands
-      .set(client.commands)
-      .then(() => {
-        message.reply('Deployed!');
-      })
-      .catch(err => {
-        message.reply('Could not deploy commands! Make sure the bot has the application.commands permission!');
-        console.error(err);
-      });
+  if (message.content === '!deploy' && message.author.id ===
+      client.application?.owner?.id) {
+    await message.guild.commands.set(client.commands).then(() => {
+      message.reply('Deployed!');
+    }).catch(err => {
+      message.reply(
+          'Could not deploy commands! Make sure the bot has the application.commands permission!');
+      console.error(err);
+    });
   }
 });
 
@@ -83,11 +87,8 @@ client.on('interactionCreate', async interaction => {
   const command = client.commands.get(interaction.commandName.toLowerCase());
 
   try {
-    if (interaction.commandName == 'ban' || interaction.commandName == 'userinfo') {
-      command.execute(interaction, client);
-    } else {
-      command.execute(interaction, player);
-    }
+    command.execute(interaction, player);
+    
   } catch (error) {
     console.error(error);
     interaction.followUp({
