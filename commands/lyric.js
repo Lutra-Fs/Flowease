3cebfa3e100512fd8d11b5607c0e2bbbbbaf64a2
Bill ZHANG <36790218+Lutra-Fs@users.noreply.github.com>
2022-11-26 20:27:35 +1100

    feat(lyric): add fallback for lyrics match system.
    add songlyrics as fallback when the lyrics is not match. (currently the
    detection is based on lyrics' length.

--- commands/lyric.js
+++ commands/lyric.js
@@ -17,26 +17,45 @@ module.exports = {
 		}
 		const track = queue.current;
 
-		async function lyricsFinder(title) {
+		async function lyricsFinder(title, author) {
 			const res = await ncmApi.search({
-				keywords: title,
+				keywords: title + ' ' + author,
 				limit: 1,
 				type: 1,
 			});
-			const lyrics = await ncmApi.lyric({
+			let lyrics = await ncmApi.lyric({
 				id: res.body.result.songs[0].id,
 			});
 			let lyricsText = lyrics.body.lrc.lyric;
 			lyricsText = lyricsText.replace(/\[.*\]/g, '');
+			// if lyrics are too short, try to get the lyrics from Musixmatch
+			if (lyricsText.length < 100) {
+				console.log('lyrics too short, trying Musixmatch');
+				const songlyrics = require('songlyrics');
+				lyrics = await songlyrics.getLyrics(title);
+				lyricsText = lyrics.lyrics.length > 0 ? lyrics.lyrics : lyricsText;
+			}
+			// replace multiple newlines with a single newline
+			lyricsText = lyricsText.replace(/\n{2,}/g, '\n');
 			return lyricsText;
 		}
 
-		const lyrics = await lyricsFinder(track.title) || 'No lyrics found!';
-		const lyricsEmbed = new EmbedBuilder()
-			.setTitle(`Lyrics for ${track.title}`)
-			.setDescription(lyrics)
-			.setColor('#FF0000');
-
-		return interaction.followUp({ embeds: [lyricsEmbed] });
+		const lyrics = await lyricsFinder(track.title, track.author) ||
+			'No lyrics found!';
+		// deal with long lyrics
+		if (lyrics.length > 2048) {
+			const embed = new EmbedBuilder()
+				.setTitle(`${track.title} - ${track.author}`)
+				.setDescription('Lyrics are too long to display here!');
+			return interaction.followUp({ embeds: [embed] });
+		}
+		else {
+			const embed = new EmbedBuilder()
+				.setTitle(`Lyrics for ${track.title}`)
+				.setDescription(lyrics)
+				.setColor('#FF0000')
+				.setTimestamp();
+			return interaction.followUp({ embeds: [embed] });
+		}
 	},
 };
